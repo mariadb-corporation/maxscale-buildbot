c50882b6a9b17d7b4f0ed584c10477ad097359cc
Roman Vlasov <roman.vlasov@fruct.org>
2018-05-21 20:57:53 +0300

    Create the cleanup builder (refs #23957)

--- buildbot-master/master/shell_scripts/run_destroy.sh
+++ buildbot-master/master/shell_scripts/run_destroy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [ "$do_not_destroy_vm" == "yes" ]; then
+if [ "$do_not_destroy_vm" = "yes" ]; then
     echo "Config marked as undestroyable, exiting"
     exit 1
 fi
-if [ "$try_already_running" == "yes" ]; then
+if [ "$try_already_running" = "yes" ]; then
     echo "Config uses permanent VM, exiting"
     exit 1
 fi
