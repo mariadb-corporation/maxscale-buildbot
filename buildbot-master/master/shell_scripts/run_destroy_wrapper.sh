cec53aeb0116644c50d565e780bf7072c6055608
Roman Vlasov <roman.vlasov@fruct.org>
2018-05-25 12:38:12 +0300

    Wrap run_destroy and run_smart_remove_lock sripts (refs #23957)

---
+++ buildbot-master/master/shell_scripts/run_destroy_wrapper.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+scripts_path=`dirname "$0"`
+eval "$scripts_path/run_destroy.sh"
+exit 0
---
+++ buildbot-master/master/shell_scripts/run_smart_remove_lock_wrapper.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+scripts_path=`dirname "$0"`
+eval "$scripts_path/run_smart_remove_lock.sh"
+exit 0
