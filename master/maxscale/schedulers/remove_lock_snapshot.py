11a0c1e07d572ec32ce71621c9c087b730020f47
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2018-06-21 11:18:03 +0300

    Use schedulers.properties module in schedulers.remove_lock_snapshot module (refs #24129)

--- master/maxscale/schedulers/remove_lock_snapshot.py
+++ master/maxscale/schedulers/remove_lock_snapshot.py
@@ -1,4 +1,6 @@
-from buildbot.plugins import util, schedulers
+from buildbot.plugins import schedulers
+from . import common
+from . import properties
 
 
 TRIGGERABLE_SCHEDULER = schedulers.Triggerable(
@@ -8,22 +10,13 @@ TRIGGERABLE_SCHEDULER = schedulers.Triggerable(
 
 MANUAL_SCHEDULER = schedulers.ForceScheduler(
     name="remove_lock_snapshot_force",
+    buttonName="Remove snapshot lock",
     builderNames=["remove_lock_snapshot"],
     codebases=[
-        util.CodebaseParameter(
-            "",
-            branch=util.FixedParameter(name="branch", default=""),
-            revision=util.FixedParameter(name="revision", default=""),
-            project=util.FixedParameter(name="project", default=""),
-            repository=util.FixedParameter(name="repository",
-                                           default=""),
-        ),
+        common.maxscale_codebase(),
     ],
     properties=[
-        util.StringParameter(name="name",
-                             label="Name of this build",
-                             default="test01",
-                             size=50),
+        properties.build_name(),
     ]
 )
 
