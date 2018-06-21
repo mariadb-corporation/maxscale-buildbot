3340ea5d4f053e4479fe16cfae026c6b468042e0
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2018-06-21 11:10:12 +0300

    Use schedulers.properties in schedulers.destroy module (refs #24129)

--- master/maxscale/schedulers/destroy.py
+++ master/maxscale/schedulers/destroy.py
@@ -1,4 +1,6 @@
-from buildbot.plugins import util, schedulers
+from buildbot.plugins import schedulers
+from . import common
+from . import properties
 
 
 TRIGGERABLE_SCHEDULER = schedulers.Triggerable(
@@ -10,27 +12,12 @@ MANUAL_SCHEDULER = schedulers.ForceScheduler(
     name="destroy_force",
     builderNames=["destroy"],
     codebases=[
-        util.CodebaseParameter(
-            "",
-            branch=util.FixedParameter(name="branch", default=""),
-            revision=util.FixedParameter(name="revision", default=""),
-            project=util.FixedParameter(name="project", default=""),
-            repository=util.FixedParameter(name="repository",
-                                           default=""),
-        ),
+        common.maxscale_codebase(),
     ],
     properties=[
-        util.StringParameter(name="name", label="Name of this build", size=50, default="test01"),
-        util.ChoiceStringParameter(
-            name="do_not_destroy_vm",
-            label="Do not destroy vm",
-            choices=['no', 'yes'],
-            default='no'),
-        util.ChoiceStringParameter(
-            name="try_already_running",
-            label="Try already running",
-            choices=["no", "yes"],
-            default="no"),
+        properties.build_name(),
+        properties.keep_virtual_machines(),
+        properties.try_already_running(),
     ]
 )
 
