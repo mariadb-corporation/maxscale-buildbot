0f6794c40b014396a0b7e43346fd74ec6b78d5de
Maksim Kosterin <maksim.kosterin@fruct.org>
2018-07-11 14:18:57 +0300

    Add nightly scheduler for 'build_all' task (refs #24172)

--- master/maxscale/schedulers/build_all.py
+++ master/maxscale/schedulers/build_all.py
@@ -1,5 +1,6 @@
 from buildbot.plugins import schedulers
 from maxscale.schedulers.build import BUILD_PROPERTIES
+from maxscale.config import constants
 from . import properties
 from . import common
 
@@ -15,4 +16,18 @@ MANUAL_SCHEDULER = schedulers.ForceScheduler(
     properties=BUILD_ALL_PROPERTIES
 )
 
-SCHEDULERS = [MANUAL_SCHEDULER]
+NIGHTLY_22_SCHEDULER = schedulers.Nightly(
+    name='nightly_22',
+    branch='2.2',
+    builderNames=['build_all'],
+    hour=14, minute=6,
+    codebases={
+        'codebase1': {
+            'branch': '2.2',
+            'repository': constants.MAXSCALE_REPOSITORY
+        }
+    },
+    properties=properties.extractDefaultValues(BUILD_ALL_PROPERTIES)
+)
+
+SCHEDULERS = [MANUAL_SCHEDULER, NIGHTLY_22_SCHEDULER]
