3501562f52a51d989a86edf51237cd2a9978a8f2
Maksim Kosterin <maksim.kosterin@fruct.org>
2018-07-17 18:26:17 +0300

    Add 'build_for_release' task to the list of tasks (refs #24207)

--- master/maxscale/builders/__init__.py
+++ master/maxscale/builders/__init__.py
@@ -5,6 +5,7 @@ from . import build_and_simple_test
 from . import run_test
 from . import run_test_snapshot
 from . import build_all
+from . import build_for_release
 
 MAXSCALE_BUILDERS = list(itertools.chain(
     build.BUILDERS,
@@ -12,4 +13,5 @@ MAXSCALE_BUILDERS = list(itertools.chain(
     run_test.BUILDERS,
     run_test_snapshot.BUILDERS,
     build_all.BUILDERS,
+    build_for_release.BUILDERS
 ))
--- master/maxscale/schedulers/__init__.py
+++ master/maxscale/schedulers/__init__.py
@@ -5,7 +5,7 @@ from . import build_and_simple_test
 from . import run_test
 from . import run_test_snapshot
 from . import build_all
-
+from . import build_for_release
 
 MAXSCALE_SCHEDULERS = list(itertools.chain(
     build.SCHEDULERS,
@@ -13,4 +13,5 @@ MAXSCALE_SCHEDULERS = list(itertools.chain(
     run_test.SCHEDULERS,
     run_test_snapshot.SCHEDULERS,
     build_all.SCHEDULERS,
+    build_for_release.SCHEDULERS
 ))
