71954d78d92c9f2f819e95542c352f4c83614c95
Maksim Kosterin <maksim.kosterin@fruct.org>
2018-07-27 18:00:31 +0300

    Add mail notifier for 'build_and_test_snapshot' task (refs #24223)

--- master/maxscale/services/__init__.py
+++ master/maxscale/services/__init__.py
@@ -3,10 +3,12 @@ from . import build
 from . import run_test
 from . import run_test_snapshot
 from . import build_all
+from . import build_and_test_shapshot
 
 MAXSCALE_SERVICES = list(itertools.chain(
     build.SERVICES,
     run_test.SERVICES,
     run_test_snapshot.SERVICES,
-    build_all.SERVICES
+    build_all.SERVICES,
+    build_and_test_shapshot.SERVICES,
 ))
