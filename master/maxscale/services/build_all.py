651a56ab5de32a688d6a3b2b12e2e09f52fb9681
Maksim Kosterin <maksim.kosterin@fruct.org>
2018-07-30 19:20:47 +0300

    Move template for build_all message to common.py (refs #24223)

--- master/maxscale/services/build_all.py
+++ master/maxscale/services/build_all.py
@@ -1,31 +1,7 @@
 from . import common
 
+MAIL_TEMPLATE = common.COMPLEX_STEPS_BUILD_TEMPLATE + '''\
 
-MAIL_TEMPLATE = u'''\
-
-    <a href="{{ build_url }}">{{ buildername }}</a>
-    <h4>Build status: {{ summary }}</h4>
-    <p> Worker used: {{ workername }}</p>
-    <br>
-    <b>Steps results:</b>
-    <ul>
-    {% for step in build['steps'] %}
-    <li>
-        {{ step['name'] }}: <b style='color:{{colors[step['results']]}}'>{{ statuses[step['results']]}}</b>
-        {% if step['triggeredBuilds'] %}
-            <ul>
-            {% for triggeredBuild in step['triggeredBuilds'] %}
-                <li>
-                    <a href="{{ triggeredBuild['build_url'] }}">{{ triggeredBuild['name'] }}</a>:
-                    <b style='color:{{colors[triggeredBuild['results']]}}'>{{ statuses[triggeredBuild['results']]}}</b>
-                </li>
-            {% endfor %}
-            <ul>
-        {% endif %}
-    </li>
-    {% endfor %}
-    </ul>
-    <br>
     <p><b> -- The Buildbot</b></p>
     '''
 
--- master/maxscale/services/common.py
+++ master/maxscale/services/common.py
@@ -47,6 +47,32 @@ TEST_TEMPLATE = BUILD_SUMMARY_HEADER + '''\
     <p><b> -- The Buildbot</b></p>
     '''
 
+COMPLEX_STEPS_BUILD_TEMPLATE = u'''\
+
+    <a href="{{ build_url }}">{{ buildername }}</a>
+    <h4>Build status: {{ summary }}</h4>
+    <p> Worker used: {{ workername }}</p>
+    <br>
+    <b>Steps results:</b>
+    <ul>
+    {% for step in build['steps'] %}
+    <li>
+        {{ step['name'] }}: <b style='color:{{colors[step['results']]}}'>{{ statuses[step['results']]}}</b>
+        {% if step['triggeredBuilds'] %}
+            <ul>
+            {% for triggeredBuild in step['triggeredBuilds'] %}
+                <li>
+                    <a href="{{ triggeredBuild['build_url'] }}">{{ triggeredBuild['name'] }}</a>:
+                    <b style='color:{{colors[triggeredBuild['results']]}}'>{{ statuses[triggeredBuild['results']]}}</b>
+                </li>
+            {% endfor %}
+            <ul>
+        {% endif %}
+    </li>
+    {% endfor %}
+    </ul>
+    <br>
+    '''
 
 RESULT_COLOR = [
     "#8d4",  # success
