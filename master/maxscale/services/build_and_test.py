607de90a0c287c15041f72c91bf7b823c2b41f7e
Maksim Kosterin <maksim.kosterin@fruct.org>
2018-08-03 12:57:56 +0300

    Change test template for 'build_and_test' task (refs #24223)

--- master/maxscale/services/build_and_test.py
+++ master/maxscale/services/build_and_test.py
@@ -28,4 +28,4 @@ MAIL_TEMPLATE = common.COMPLEX_STEPS_BUILD_TEMPLATE + '''\
     <p><b> -- The Buildbot</b></p>
     '''
 
-SERVICES = [common.create_mail_notifier(common.TEST_TEMPLATE, ['build_and_test'])]
+SERVICES = [common.create_mail_notifier(MAIL_TEMPLATE, ['build_and_test'])]
