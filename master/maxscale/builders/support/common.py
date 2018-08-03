f2682e97a1c6824e9b0a0ae4bdd215abeb49da39
Maksim Kosterin <maksim.kosterin@fruct.org>
2018-08-03 10:51:25 +0300

    Dynamically seek for remote worker's hosts in the buildmaster's connections (refs #24119)

--- master/maxscale/builders/support/common.py
+++ master/maxscale/builders/support/common.py
@@ -3,6 +3,7 @@ from buildbot.plugins import util, steps
 from buildbot.process.buildstep import ShellMixin
 from buildbot.process.results import SKIPPED
 from buildbot.steps.shell import ShellCommand
+from buildbot.steps.shellsequence import ShellSequence
 from twisted.internet import defer
 from maxscale.builders.support import support
 from maxscale.config import workers
@@ -195,17 +196,31 @@ def generateRepositories():
     )]
 
 
-@util.renderer
-def createRsyncQueue(properties):
-    return [util.ShellArg(command="rsync -r ~/.config/mdbci/repo.d "
-                                  "vagrant@{}:~/.config/mdbci/repo.d".format(worker['host']),
-                          logfile="rsync to {}".format(worker['host']))
-            for worker in workers.WORKER_CREDENTIALS
-            if worker['name'] is not properties.getProperty("workername")]
-
-
 def syncRepod():
-    return [steps.ShellSequence(
-        name="Synchronizing ~/.config/mdbci/repo.d among workers",
-        commands=createRsyncQueue
-    )]
+    return [RsyncShellSequence(name="Synchronizing ~/.config/mdbci/repo.d among workers")]
+
+
+class RsyncShellSequence(ShellSequence):
+    """
+    rsync ~/.config/mdbci/repo.d directory from current worker
+    to every other unique worker's host
+    """
+    def createRsyncSequence(self, hosts):
+        return [util.ShellArg(command="rsync -r ~/.config/mdbci/repo.d "
+                                      "vagrant@{}:~/.config/mdbci/repo.d".format(host),
+                              logfile="rsync to {}".format(host)) for host in hosts]
+
+    def getRemoteWorkersHosts(self):
+        hosts = set()
+        for worker in self.master.workers.workers:
+            if worker is not self.getProperty("workername"):
+                connection = self.master.workers.connections.get(worker)
+                if connection:
+                    hosts.add(connection.mind.broker.transport.getPeer().host)
+        hosts.discard('127.0.0.1')
+        return hosts
+
+    def run(self):
+        hosts = self.getRemoteWorkersHosts()
+        self.commands = self.createRsyncSequence(hosts)
+        return self.runShellSequence(self.commands)
