0b537f26261f183b80b2900f129e7f3a42e1eee2
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2018-08-07 13:18:59 +0300

    Add methods to determine IP-address by hosts and check for domains (refs #24275)

--- worker-management/manage.py
+++ worker-management/manage.py
@@ -4,21 +4,64 @@ import sys
 import os.path
 import argparse
 import getpass
+import socket
 sys.path.append(os.path.abspath("{}/../../master/".format(__file__)))
 import maxscale.config.workers as workers
 
 
-def parse_arguments():
+def parseArguments():
     parser = argparse.ArgumentParser(description="A tool to install, restart the BuildBot worker instances.")
     parser.add_argument("action", help="Action to perform, install for example.")
     parser.add_argument("--host", help="Host to manage.")
     parser.add_argument("--user", help="User to use during the SSH connection to host.", default=getpass.getuser())
+    parser.add_argument("--domain", help="Default domain for hosts", default="mariadb.com")
     return parser.parse_args()
 
 
+def determineIP(host, domain):
+    possibleHosts = [
+        host,
+        "{}.{}".format(host, domain)
+    ]
+    for checkHost in possibleHosts:
+        try:
+            info = socket.gethostbyname(checkHost)
+        except BaseException:
+            continue
+        return info
+    return None
+
+
+def determineHosts(arguments):
+    hosts = []
+    for hostConfiguration in workers.WORKER_CREDENTIALS:
+        if arguments.host is not None and hostConfiguration["host"] != arguments.host:
+            continue
+        ipAddress = determineIP(hostConfiguration["host"], arguments.domain)
+        if ipAddress is None:
+            continue
+        hosts.append({
+            "ip": ipAddress,
+            "config": hostConfiguration
+        })
+    return hosts
+
+def installWorkers(hosts):
+    print(hosts)
+    pass
+
+
 def main():
-    arguments = parse_arguments()
-    print(arguments)
+    arguments = parseArguments()
+    actions = {
+        'install': installWorkers
+    }
+    action = actions.get(arguments.action)
+    if action is None:
+        print("Unknown action '{}'.".format(arguments.action))
+        exit(1)
+    hosts = determineHosts(arguments)
+    action(hosts)
 
 
 if __name__ == "__main__":
