e0b0db21ef37b4c8a669a0e1d63b65ff9c06d614
Maksim Kosterin <maksim.kosterin@fruct.org>
2018-08-17 16:45:44 +0300

    Add custom module finder and loader (refs #24152)

--- master/autoreload.py
+++ master/autoreload.py
@@ -1,16 +1,74 @@
+import os
 import sys
-import builtins
-import importlib
 from importlib.abc import Loader
+from importlib.abc import MetaPathFinder
+from importlib.util import spec_from_file_location
+
+
+class ReMetaPathFinder(MetaPathFinder):
+
+    def find_spec(self, fullname, path, target=None):
+        if not fullname.startswith("maxscale"):
+            return None
+        if not path:
+            path = [os.getcwd()]
+        if "." in fullname:
+            *parents, name = fullname.split(".")
+        else:
+            name = fullname
+        for entry in path:
+            if os.path.isdir(os.path.join(entry, name)):
+                filename = os.path.join(entry, name, "__init__.py")
+                submodule_locations = [os.path.join(entry, name)]
+            else:
+                filename = os.path.join(entry, name + ".py")
+                submodule_locations = None
+            if not os.path.exists(filename):
+                continue
+            return spec_from_file_location(fullname, filename, loader=ReLoader(),
+                                           submodule_search_locations=submodule_locations)
+        return None
 
 
 class ReLoader(Loader):
 
-    def create_module(self, spec):
-        pass
-
     def exec_module(self, module):
-        pass
+        with open(module.__file__, "r") as file:
+            code = file.read()
+            exec(code, module.__dict__)
+        maxscale_modules.append(module.__name__)
+        return module
 
 
-modules = {}
+def install():
+    for cls in sys.meta_path:
+        if isinstance(cls, ReMetaPathFinder):
+            del cls
+
+    while maxscale_modules:
+        mod = maxscale_modules.pop()
+        if mod in sys.modules:
+            del sys.modules[mod]
+        else:
+            print("{} not in the sys.modules".format(mod))
+
+    sys.meta_path.insert(0, ReMetaPathFinder())
+
+
+maxscale_modules = []
+
+
+"""
+import builtins
+
+
+real_import = builtins.__import__
+
+
+def _import(name, globals=None, locals=None, fromlist=(), level=0):
+    print(name)
+    return real_import(name, globals, locals, fromlist, level)
+
+
+builtins.__import__ = _import
+"""
