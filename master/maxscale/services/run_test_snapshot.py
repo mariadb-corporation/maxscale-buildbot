beed9778f444b159845f72b695be611a69a542d1
Maksim Kosterin <maksim.kosterin@fruct.org>
2018-09-17 13:43:34 +0300

    Determine if email notification is needed based on the way build was triggered (closes #24349)

--- master/maxscale/services/run_test_snapshot.py
+++ master/maxscale/services/run_test_snapshot.py
@@ -1,5 +1,3 @@
 from . import common
-from maxscale.schedulers.run_test_snapshot import MANUAL_SCHEDULER
 
-SERVICES = [common.create_mail_notifier(common.TEST_TEMPLATE, ['run_test_snapshot'],
-                                        schedulers=[MANUAL_SCHEDULER.name])]
+SERVICES = [common.create_mail_notifier(common.TEST_TEMPLATE, ['run_test_snapshot'])]
