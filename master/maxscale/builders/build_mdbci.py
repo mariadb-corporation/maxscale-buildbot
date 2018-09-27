f3b71f9163532eeabc7d7b9c00648c98db0865c6
Timofey Turenko <timofey.turenko@mariadb.com>
2018-09-27 18:06:59 +0300

    add copying of MDBCI to ci repo

--- master/maxscale/builders/build_mdbci.py
+++ master/maxscale/builders/build_mdbci.py
@@ -4,6 +4,7 @@ from buildbot.config import BuilderConfig
 from buildbot.plugins import util, steps
 from maxscale import workers
 from maxscale.builders.support import common, support
+from buildbot.steps.shell import ShellCommand
 
 ENVIRONMENT = {
     "JOB_NAME": util.Property("buildername"),
@@ -22,8 +23,15 @@ def configureBuildProperties(properties):
 def remoteBuildMdbci():
     """This script will be run on the worker"""
     os.chdir("../build/package")
-    results = subprocess.run(["./build.sh 1"], shell=True)
+    results = subprocess.run("./build.sh " + str(buildnumber), shell=True)
     sys.exit(results.returncode)
+
+
+def publishMdbci():
+    return [steps.ShellCommand(
+        name="Copy MDBCI AppImage to CI repository",
+        command=util.Interpolate("cp %(prop:builddir)s/build/package/build/out/* $HOME/%(prop:repo_path)s"),
+        alwaysRun=False)]
 
 
 def createBuildSteps():
@@ -32,8 +40,8 @@ def createBuildSteps():
     buildSteps.extend(common.cloneRepository())
     buildSteps.extend(support.executePythonScript(
         "Build MDBCI", remoteBuildMdbci))
+    buildSteps.extend(publishMdbci())
     buildSteps.extend(common.cleanBuildDir())
-    buildSteps.extend(common.destroyVirtualMachine())
     return buildSteps
 
 
