52b0414fb1e3bb70da28c3f0094f191e8da296d2
Timofey Turenko <timofey.turenko@mariadb.com>
2018-09-28 23:12:56 +0300

    Mxs 1863 build mdbci (#8)
    * add build_mdbci
    
    * add build_mdbci to __init
    
    * fix build_mdbci scheduler name
    
    * fix path in build_mdbci
    
    * add shell=True
    
    * add copying of MDBCI to ci repo
    
    * add symlink creation from 'mdbci' to the latest build
    
    * use build_mdbci tag for MDBCI builder

---
+++ master/maxscale/builders/build_mdbci.py
@@ -0,0 +1,71 @@
+import os
+
+from buildbot.config import BuilderConfig
+from buildbot.plugins import util, steps
+from maxscale import workers
+from maxscale.builders.support import common, support
+from buildbot.steps.shell import ShellCommand
+
+ENVIRONMENT = {
+    "JOB_NAME": util.Property("buildername"),
+    "BUILD_ID": util.Interpolate('%(prop:buildnumber)s'),
+    "BUILD_NUMBER": util.Interpolate('%(prop:buildnumber)s'),
+}
+
+
+@util.renderer
+def configureBuildProperties(properties):
+    return {
+        "mdbciConfig": util.Interpolate("%(prop:buildername)s-%(prop:buildnumber)s")
+    }
+
+
+def remoteBuildMdbci():
+    """This script will be run on the worker"""
+    os.chdir("../build/package")
+    results = subprocess.run("./build.sh " + str(buildnumber), shell=True)
+    sys.exit(results.returncode)
+
+
+def publishMdbci():
+    return [steps.ShellCommand(
+        name="Copy MDBCI AppImage to CI repository",
+        command=util.Interpolate(
+            "cp %(prop:builddir)s/build/package/build/out/* \
+             $HOME/%(prop:repo_path)s; \
+             mdbci_file=`ls %(prop:builddir)s/build/package/build/out/* \
+             | xargs -n1 basename`; unlink $HOME/%(prop:repo_path)s/mdbci; \
+             ln -s $HOME/%(prop:repo_path)s/${mdbci_file} \
+             $HOME/%(prop:repo_path)s/mdbci"),
+        alwaysRun=False)]
+
+
+def createBuildSteps():
+    buildSteps = []
+    buildSteps.append(steps.SetProperties(properties=configureBuildProperties))
+    buildSteps.extend(common.cloneRepository())
+    buildSteps.extend(support.executePythonScript(
+        "Build MDBCI", remoteBuildMdbci))
+    buildSteps.extend(publishMdbci())
+    buildSteps.extend(common.cleanBuildDir())
+    return buildSteps
+
+
+def createBuildFactory():
+    factory = util.BuildFactory()
+    buildSteps = createBuildSteps()
+    factory.addSteps(buildSteps)
+    return factory
+
+
+BUILDERS = [
+    BuilderConfig(
+        name="build_mdbci",
+        workernames=workers.workerNames(),
+        factory=createBuildFactory(),
+        nextWorker=common.assignWorker,
+        tags=["build_mdbci"],
+        env=ENVIRONMENT,
+        collapseRequests=False,
+    )
+]
