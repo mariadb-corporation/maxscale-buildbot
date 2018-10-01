a2821d18191547af09b31e5cc61196b9208a6038
Maksim Kosterin <maksim.kosterin@fruct.org>
2018-10-01 14:10:34 +0300

    Recreate parseCtestLog function (refs #24385)

--- master/maxscale/builders/support/parse_ctest_log.py
+++ master/maxscale/builders/support/parse_ctest_log.py
@@ -1,4 +1,5 @@
 import os
+import re
 import sys
 import argparse
 
@@ -84,18 +85,94 @@ MAXSCALE_FULL = "Maxscale full version"
 NEW_LINE_JENKINS_FORMAT = " \\n\\\n"
 
 
-parser = argparse.ArgumentParser(description="CTest parser usage:")
-parser.add_argument(LOG_FILE_OPTION, help="CTEST LOG FILE PATH")
-parser.add_argument("-f", ONLY_FAILED_OPTION, action="store_true", help="PARSE ONLY FAILED TESTS")
-parser.add_argument("-r", HUMAN_READABLE_OPTION, action="store_true", help="HUMAN READABLE OUTPUT")
-parser.add_argument("-o", OUTPUT_LOG_FILE_OPTION, metavar="file_path",
-                    help="CTEST PARSER OUTPUT LOG FILE HUMAN READABLE FOR JENKINS (environmental variable format)")
-parser.add_argument("-j", OUTPUT_LOG_JSON_FILE_OPTION, metavar="json_file_path",
-                    help="CTEST PARSER OUTPUT LOG JSON FILE (there will be saved all test results - passed and failed)")
+options = argparse.ArgumentParser(description="CTest parser usage:")
+options.add_argument(LOG_FILE_OPTION, help="CTEST LOG FILE PATH")
+options.add_argument("-f", ONLY_FAILED_OPTION, action="store_true", help="PARSE ONLY FAILED TESTS")
+options.add_argument("-r", HUMAN_READABLE_OPTION, action="store_true", help="HUMAN READABLE OUTPUT")
+options.add_argument("-o", OUTPUT_LOG_FILE_OPTION, metavar="file_path",
+                     help="CTEST PARSER OUTPUT LOG FILE HUMAN READABLE "
+                          "FOR JENKINS (environmental variable format)")
+options.add_argument("-j", OUTPUT_LOG_JSON_FILE_OPTION, metavar="json_file_path",
+                     help="CTEST PARSER OUTPUT LOG JSON FILE (there will be "
+                          "saved all test results - passed and failed)")
+
+
+class CTestParser:
+
+    def __init__(self, args):
+        self.args = args
+        self.ctestExecuted = False
+        self.ctestSummary = None
+        self.maxscaleCommit = None
+        self.cmakeFlags = None
+        self.maxscaleSource = None
+        self.logsDir = None
+        self.allCtestIndexes = []
+        self.failedCtestIndexes = []
+        self.allCtestArguments = None
+        self.failedCtestArguments = None
+        self.allCtestInfo = {}
+        self.failedCtestInfo = {}
+        self.failCtestCounter = 0
+        self.maxscaleEntity = []
+
+    def parseCtestLog(self):
+        ctestFirstLineRegex = re.compile("Constructing a list of tests")
+        ctestLastLineRegex = re.compile("tests passed,.+tests failed out of (.+)")
+        maxscaleCommitRegex = re.compile(r"MaxScale\s+.*\d+\.*\d*\.*\d*\s+-\s+(.+)")
+        cmakeFlagsRegex = re.compile(r"CMake flags:\s+(.+)")
+        maxscaleSourceRegex = re.compile(r"Source:\s+(.+)")
+        logsFirRegex = re.compile(r"^Logs go to \/home\/vagrant\/LOGS\/(.+)$")
+        maxscaleVersionStartRegex = re.compile(".*Maxscale_full_version_start:.*")
+        maxscaleVersionEndRegex = re.compile(".*Maxscale_full_version_end.*")
+        ctestStartLine = 0
+        maxscaleVersionStartFound = False
+        maxscaleVersionEndFound = False
+
+        file = open(self.args.log_file, encoding="UTF-8")
+        for line in file:
+            if maxscaleVersionEndRegex.match(line):
+                maxscaleVersionEndFound = True
+            if maxscaleVersionStartFound and not maxscaleVersionEndFound and line.replace("\n", ""):
+                self.maxscaleEntity.append(line.replace("\n", ""))
+            if maxscaleVersionStartRegex.match(line):
+                maxscaleVersionStartFound = True
+            if maxscaleCommitRegex.match(line) and not self.maxscaleCommit:
+                self.maxscaleCommit = maxscaleCommitRegex.match(line)[0]
+            if maxscaleSourceRegex.match(line) and not self.maxscaleSource:
+                self.maxscaleSource = maxscaleSourceRegex.match(line)[0].strip()
+            if logsFirRegex.match(line) and not self.logsDir:
+                self.logsDir = logsFirRegex.match(line)[0].strip()
+            if ctestFirstLineRegex:
+                self.ctestExecuted = True
+                break
+            ctestStartLine += 1
+
+        if self.ctestExecuted:
+            ctestLog = file.readlines()[ctestStartLine:-1]
+            ctestEndLine = 0
+            for line in ctestLog:
+                if ctestLastLineRegex.match(line):
+                    self.ctestSummary = line
+                    break
+                ctestEndLine += 1
+            ctestLog = ctestLog[:ctestEndLine]
+            testQuantity = ctestLastLineRegex.match(ctestLog[-1])[0]
+            self.findTestsInfo(ctestLog)
+            self.allCtestInfo.update({TESTS_COUNT: testQuantity})
+            self.failedCtestInfo.update({TESTS_COUNT: testQuantity})
+        else:
+            self.ctestSummary = CTEST_SUMMARY_NOTE_FOUND
+            self.allCtestInfo = {TESTS_COUNT: NOT_FOUND, FAILED_TESTS_COUNT: NOT_FOUND, TESTS: []}
+            self.failedCtestInfo = {TESTS_COUNT: NOT_FOUND, FAILED_TESTS_COUNT: NOT_FOUND, TESTS: []}
+
+    def findTestsInfo(self, ctestLog):
+
 
 
 def main():
-    args = parser.parse_args()
+    args = options.parse_args()
+    CTestParser(args)
 
 
 if os.path.samefile(__file__, sys.argv[0]):
