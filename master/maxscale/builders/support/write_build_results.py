ee1a1bc3c9cec55b1a81d6d7ef834bc9373f891f
Maksim Kosterin <maksim.kosterin@fruct.org>
2018-10-02 17:02:25 +0300

    Recreate functions for inserting in test_run and results tables (refs #24385)

--- master/maxscale/builders/support/write_build_results.py
+++ master/maxscale/builders/support/write_build_results.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+import json
 import os
 import re
 import sys
 import subprocess
 import argparse
-import mysql
+import mysql.connector
+from configparser import ConfigParser
 
 
 # Command line options
@@ -41,7 +43,57 @@ class BuildResultsWriter:
         self.parsedContent = None
 
     def writeResultsFromInputFile(self, inputFilePath):
-        pass
+        self.parseInputFile(inputFilePath)
+        self.connectMdb(DEFAULT_FILE, DB_NAME)
+        self.writeBuildResultsToDb(self.parsedContent)
+
+    def parseInputFile(self, inputFilePath):
+        with open(inputFilePath, "r") as file:
+            self.parsedContent = json.load(" ".join(file.readline()))
+
+    def connectMdb(self, defaultFile, dbName):
+        self.client = mysql.connector.connect(database=dbName, **self.readDatabaseConfiguration(defaultFile))
+        print("Successfully connected to database {} using configuration {}"
+              .format(dbName, defaultFile))
+
+    def readDatabaseConfiguration(self, filename, section="client"):
+        config = ConfigParser()
+        config.read(filename)
+        db = {}
+        if config.has_section(section):
+            for item in config.items(section):
+                db[item[0]] = item[1]
+        else:
+            raise Exception('{0} not found in the {1} file'.format(section, filename))
+
+    def writeTestRunTable(self, jenkinsId, startTime, targat, box, product, mariadbVersion,
+                          testCodeCommitId, maxscaleCommitId, jobName,
+                          cmakeFlags, maxscaleSource, logsDir):
+        cursor = self.client.cursor()
+        query = ("INSERT INTO test_run (jenkins_id, "
+                 "start_time, target, box, product, mariadb_version, "
+                 "test_code_commit_id, maxscale_commit_id, job_name, "
+                 "cmake_flags, maxscale_source, logs_dir) "
+                 "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
+        values = (jenkinsId, startTime, targat, box, product, mariadbVersion,
+                  testCodeCommitId, maxscaleCommitId, jobName,
+                  cmakeFlags, maxscaleSource, logsDir)
+        cursor.execute(query, values)
+        id = cursor.lastrowid
+        cursor.commit()
+        cursor.close()
+        print("Performed insert (test_run, id = {}: {}".format(id, query % values))
+        return id
+
+    def writeResultsTable(self, id, test, result, testTime, coreDumpPath):
+        cursor = self.client.cursor()
+        query = ("INSERT INTO results (id, test, result, test_time, core_dump_path) "
+                 "VALUES (%s, %s, %s, %s, %s)")
+        values = (id, test, result, testTime, coreDumpPath)
+        cursor.execute(query, values)
+        cursor.commit()
+        cursor.close()
+        print("Performed insert (results): {}".format(query % values))
 
 
 def main():
@@ -53,7 +105,7 @@ def main():
         print(e.__cause__)
         print(e.__traceback__)
         if args.env_file:
-            with open(args.env_file, "rw") as file:
+            with open(args.env_file, "w") as file:
                 file.write("{} {}".format(DB_WRITE_ERROR, e.__cause__))
 
 
