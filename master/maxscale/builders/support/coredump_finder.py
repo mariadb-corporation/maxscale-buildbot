bf60e9c7cbfe7f462381528e45cc33ad8e467009
Maksim Kosterin <maksim.kosterin@fruct.org>
2018-10-04 16:28:48 +0300

    Move body of coredump finder int a function (refs #24385)

--- master/maxscale/builders/support/coredump_finder.py
+++ master/maxscale/builders/support/coredump_finder.py
@@ -9,21 +9,26 @@ options = argparse.ArgumentParser(description="Core dump finder")
 options.add_argument("build_id", help="Id of the build (<build_name>_<build_number>)")
 options.add_argument("output_format", help="Output format (url|files)", choices=["url", "files"])
 
-args = options.parse_args()
-HOME = os.environ["HOME"]
-logsPath = "{}/LOGS".format(HOME)
-buildPath = "{}/{}".format(logsPath, args.build_id)
 
-if not os.path.isdir(buildPath):
-    print("Directory {} does not exist, exiting.".format(buildPath))
-    sys.exit(1)
+def main(args=None):
+    args = options.parse_args(args=args)
+    HOME = os.environ["HOME"]
+    logsPath = "{}/LOGS".format(HOME)
+    buildPath = "{}/{}".format(logsPath, args.build_id)
 
-if args.output_format == "url":
-    sys.exit(subprocess.run(['find {} | grep core | sed -e "s|{}/|http://max-tst-01.mariadb.com/|"'
-                            .format(buildPath, HOME)], shell=True).returncode)
+    if not os.path.isdir(buildPath):
+        print("Directory {} does not exist, exiting.".format(buildPath))
+        sys.exit(1)
 
-pwd = os.getcwd()
-os.chdir(buildPath)
-subprocess.run([r"find ./ | grep core | sed -e 's|/[^/]*$|/*|g'"], shell=True)
-os.chdir(pwd)
-sys.exit(0)
+    if args.output_format == "url":
+        sys.exit(subprocess.run(['find {} | grep core | sed -e "s|{}/|http://max-tst-01.mariadb.com/|"'
+                                .format(buildPath, HOME)], shell=True).returncode)
+
+    pwd = os.getcwd()
+    os.chdir(buildPath)
+    subprocess.run([r"find ./ | grep core | sed -e 's|/[^/]*$|/*|g'"], shell=True)
+    os.chdir(pwd)
+
+
+if os.path.samefile(__file__, sys.argv[0]):
+    main()
