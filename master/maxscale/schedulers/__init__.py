1e6955d303146952025e97a90b45246ec7b1444f
Timofey Turenko <timofey.turenko@mariadb.com>
2018-10-18 09:22:08 +0000

    add build_and_performance_test to __init__.py

--- master/maxscale/schedulers/__init__.py
+++ master/maxscale/schedulers/__init__.py
@@ -1,6 +1,7 @@
 import itertools
 from . import build
 from . import build_and_test
+from . import build_and_performance_test
 from . import build_and_test_snapshot
 from . import run_test
 from . import run_test_snapshot
@@ -15,6 +16,7 @@ from . import build_mdbci
 MAXSCALE_SCHEDULERS = list(itertools.chain(
     build.SCHEDULERS,
     build_and_test.SCHEDULERS,
+    build_and_performance_test.SCHEDULERS,
     run_test.SCHEDULERS,
     run_test_snapshot.SCHEDULERS,
     build_all.SCHEDULERS,
