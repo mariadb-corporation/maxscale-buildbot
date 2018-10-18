9fb6447ee238e2fa35ea67de5b05dca87c362900
Timofey Turenko <timofey.turenko@mariadb.com>
2018-10-18 20:22:32 +0000

    add nightly performance tests

--- master/maxscale/schedulers/build_and_performance_test.py
+++ master/maxscale/schedulers/build_and_performance_test.py
@@ -26,23 +26,23 @@ SCHEDULERS = [MANUAL_SCHEDULER]
 # Add schedulers for every active branch to be built every night
 # The list of branches is defined by constants.NIGHTLY_SCHEDS
 # (see maxscale/config/constants.py)
-#BUILD_INTERVAL = 3
-#launchTime = 20
-#for branch in constants.NIGHTLY_SCHEDS:
-#    nightlyProperties = properties.extractDefaultValues(BUILD_AND_TEST_PROPERTIES)
-#    nightlyProperties["name"] = "nightly_test_{}".format(branch)
-#    nightlyProperties['owners'] = constants.NIGHTLY_MAIL_LIST
-#    del nightlyProperties["target"]
+BUILD_INTERVAL = 3
+launchTime = 18
+for branch in constants.NIGHTLY_SCHEDS:
+    nightlyProperties = properties.extractDefaultValues(BUILD_AND_PERFORMANCE_TEST_PROPERTIES)
+    nightlyProperties["name"] = "nightly_test_{}".format(branch)
+    nightlyProperties['owners'] = constants.NIGHTLY_MAIL_LIST
+    del nightlyProperties["target"]
 
-#    nightlyScheduler = schedulers.Nightly(
-#        name="build_and_test_{}_nightly".format(branch),
-#        builderNames=["build_and_test"],
-#        hour=launchTime % 24, minute=1,
-#        codebases={"": {
-#            "branch": branch,
-#            "repository": constants.MAXSCALE_REPOSITORY
-#        }},
-#        properties=nightlyProperties
-#    )
-#    SCHEDULERS.append(nightlyScheduler)
-#    launchTime += BUILD_INTERVAL
+    nightlyScheduler = schedulers.Nightly(
+        name="build_and_performance_test_{}_nightly".format(branch),
+        builderNames=["build_and_test"],
+        hour=launchTime % 24, minute=1,
+        codebases={"": {
+            "branch": branch,
+            "repository": constants.MAXSCALE_REPOSITORY
+        }},
+        properties=nightlyProperties
+    )
+    SCHEDULERS.append(nightlyScheduler)
+    launchTime += BUILD_INTERVAL
