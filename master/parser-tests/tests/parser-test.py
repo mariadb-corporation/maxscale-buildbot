cd915f16588356bd0982437d0f0f79bcf6e8c9bb
Maksim Kosterin <maksim.kosterin@fruct.org>
2018-10-24 17:38:39 +0300

    Add test for dtabase writer scripts (refs #24461)

--- master/parser-tests/tests/parser-test.py
+++ master/parser-tests/tests/parser-test.py
@@ -2,11 +2,13 @@
 
 import collections
 import json
+import mysql.connector
 import os
 import pytest
 import re
 import shutil
 import subprocess
+from configparser import ConfigParser
 from itertools import zip_longest
 
 
@@ -64,7 +66,60 @@ def readLogsDir(source):
                 return logsDirRegex.search(line).group(1).strip()
 
 
+def readDatabaseConfiguration(filename='/home/maksim/build_parser_db_password', section='client'):
+    config = ConfigParser()
+    config.read(filename)
+    db = {}
+    if config.has_section(section):
+        for item in config.items(section):
+            db[item[0]] = item[1]
+    else:
+        raise Exception("{} not found in the {} file".format(section, filename))
+    return db
+
+
 resultsDir = "{}/results".format(os.path.dirname(os.path.abspath(__file__)))
+
+
+@pytest.fixture(scope="module")
+def database():
+    dbName = 'parser_test_results_db'
+    config = readDatabaseConfiguration()
+    testdb = mysql.connector.connect(**config)
+    cursor = testdb.cursor()
+    cursor.execute("use {}".format(dbName))
+    cursor.execute('CREATE TABLE IF NOT EXISTS test_run (id int(11) PRIMARY KEY, jenkins_id int(11), '
+                   'start_time datetime, target varchar(256), box varchar(256), product varchar(256), '
+                   'mariadb_version varchar(256), test_code_commit_id varchar(256), maxscale_commit_id varchar(256), '
+                   'job_name varchar(256), cmake_flags text, maxscale_source varchar(256), logs_dir varchar(256))')
+
+    cursor.execute('CREATE TABLE IF NOT EXISTS results(id int(11), FOREIGN KEY (id) REFERENCES test_run(id) '
+                   'ON DELETE CASCADE, test varchar(256), result int(11), '
+                   'test_time float, core_dump_path varchar(500))')
+
+    cursor.close()
+    yield testdb
+    testdb.close()
+
+
+@pytest.fixture(scope="module")
+def resultsTableColumns(database):
+    cursor = database.cursor()
+    cursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS "
+                   "WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'results'")
+    columns = cursor.fetchall()
+    cursor.close()
+    return [column[0] for column in columns]
+
+
+@pytest.fixture(scope="module")
+def testRunTableColumns(database):
+    cursor = database.cursor()
+    cursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS "
+                   "WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'test_run'")
+    columns = cursor.fetchall()
+    cursor.close()
+    return [column[0] for column in columns]
 
 
 def setup_module(module):
@@ -116,3 +171,40 @@ def test_compareCtestSublogs(buildId, parseSource):
     result, message = compareDirectories("{}/{}/ruby/ctest_sublogs".format(resultsDir, buildId),
                                          "{}/{}/python/ctest_sublogs".format(resultsDir, buildId))
     assert result, message
+
+#
+# def test_compareDatabaseResults(buildId, parseSource, database, resultsTableColumns, testRunTableColumns):
+#     if not os.path.exists("{}/{}/".format(resultsDir, buildId)):
+#         pytest.skip("Test run failed and didn't produces any results")
+#
+#     cursor = database.cursor()
+#     scriptsPath = "{}/../parser/".format(os.path.dirname(os.path.abspath(__file__)))
+#
+#     subprocess.check_output(["{}/ruby-scripts/write_build_results.rb".format(scriptsPath),
+#                              "-f", "{}/{}/ruby/json".format(resultsDir, buildId)])
+#     cursor.execute("SELECT {} FROM results".format(", ".join(resultsTableColumns)))
+#     rubyResults = cursor.fetchall()
+#     cursor.execute("SELECT {} FROM test_run".format(", ".join(testRunTableColumns)))
+#     rubyTestRun = cursor.fetchall()
+#     cursor.execute('DELETE FROM test_run')
+#     database.commit()
+#
+#     subprocess.check_output(["{}/python-scripts/write_build_results.py".format(scriptsPath),
+#                              "{}/{}/ruby/json".format(resultsDir, buildId)])
+#     cursor.execute("SELECT {} FROM results".format(", ".join(resultsTableColumns)))
+#     pythonResults = cursor.fetchall()
+#     cursor.execute("SELECT {} FROM test_run".format(", ".join(testRunTableColumns)))
+#     pythonTestRun = cursor.fetchall()
+#     cursor.execute('DELETE FROM test_run')
+#     database.commit()
+#     assert rubyTestRun == pythonTestRun, \
+#         "test_run rows created by Ruby parser:\n{}\n" \
+#         "does not match rows created by Python parser:\n{}"\
+#         .format(rubyTestRun, pythonTestRun)
+#
+#     assert rubyResults == pythonResults, \
+#         "results rows created by Ruby parser:\n{}\n" \
+#         "does not match rows created by Python parser:\n{}"\
+#         .format(rubyResults, pythonResults)
+#
+#     cursor.close()
