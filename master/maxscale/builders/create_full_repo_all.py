c6c3c7b2947c44c15e0de3e46cb88ded19cb7c0c
Evgeny Vlasov <evgeny.vlasov@fruct.org>
2019-08-13 14:43:00 +0300

    Add the create_full_repo_all task (refs #25591)

--- master/maxscale/builders/create_full_repo_all.py
+++ master/maxscale/builders/create_full_repo_all.py
@@ -5,34 +5,31 @@ from maxscale.builders.support import common
 from .build import ENVIRONMENT
 
 
-def createBuildFactory():
+def createCreateFullRepoFactory():
     """
-    Creates build factory containing steps
-    which triggers build scheduler for each chosen box
+    Creates create factory containing steps
+    which triggers create_full_repo scheduler for each chosen box
     """
     factory = util.BuildFactory()
     factory.addStep(common.BuildAllTrigger(
-        name="build_all",
-        schedulerNames=['build'],
+        name="create_full_repo_all",
+        schedulerNames=['create_full_repo'],
         waitForFinish=True,
         copy_properties=[
             "branch",
-            "build_box_checkbox_container",
-            "build_experimental",
-            "ci_url",
-            "cmake_flags",
-            "do_not_destroy_vm",
             "host",
-            "old_target",
             "owners",
             "repository",
-            "run_upgrade_test",
-            "target",
-            "try_already_running",
             "version",
+            "box",
+            "target",
+            "do_not_destroy_vm",
+            "ci_url",
+            "major_ver",
+            "build_box_checkbox_container"
         ],
         set_properties={
-            "virtual_builder_name": "build"
+            "virtual_builder_name": "create_full_repo"
         }
     ))
     return factory
@@ -40,12 +37,12 @@ def createBuildFactory():
 
 BUILDERS = [
     BuilderConfig(
-        name="build_all",
+        name="create_full_repo_all",
         workernames=workers.workerNames(),
         nextWorker=common.assignWorker,
         nextBuild=common.assignBuildRequest,
-        factory=createBuildFactory(),
-        tags=["build"],
+        factory=createCreateFullRepoFactory(),
+        tags=["create_full_repo"],
         env=ENVIRONMENT,
         collapseRequests=False
     )
