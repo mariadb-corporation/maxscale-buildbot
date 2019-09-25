de8f0a677e1aa2a95fa7cb7011219a00653d1912
Timofey Turenko <timofey.turenko@mariadb.com>
2019-09-25 13:26:06 +0300

    Use 'build_and_test' instead of 'build_and_test_snapshot' for on_push tests
    build_and_test_snapshot fails if already running VMs are prerared for different version of Maxscale.
    To avoid this problem let's use 'build_and_test' for all tests triggered by push

--- master/maxscale/schedulers/__init__.py
+++ master/maxscale/schedulers/__init__.py
@@ -20,6 +20,7 @@ from . import build_docker_image
 MAXSCALE_SCHEDULERS = list(itertools.chain(
     build.SCHEDULERS,
     build_and_test.SCHEDULERS,
+    build_and_test_on_push.SCHEDULERS,
     build_and_performance_test.SCHEDULERS,
     run_test.SCHEDULERS,
     run_test_snapshot.SCHEDULERS,
