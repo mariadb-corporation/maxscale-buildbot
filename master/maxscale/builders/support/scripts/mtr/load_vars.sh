b739e688c22c320c53e18ec2e68db81af5862481
Timofey Turenko <timofey.turenko@mariadb.com>
2020-01-22 14:57:33 +0200

    remove passord

--- master/maxscale/builders/support/scripts/mtr/load_vars.sh
+++ master/maxscale/builders/support/scripts/mtr/load_vars.sh
@@ -1,5 +1,5 @@
 export packages_dir=${packages_dir:-$HOME/es_packages/}
-export REGISTRY_PASS=8e7ac6ad257ef5788d50dc1db4a3e3bd2ac
+export REGISTRY_PASS=`cat .config/mdbci/docker-reg`
 export Image=${Image:-"rhel-8"}
 export BuildType=${BuildType:-"RelWithDebInfo"}
 export mtrParam=${mtrParam:-"--mtr-normal-test"}
