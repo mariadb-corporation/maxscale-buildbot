998bd0ed8c0b45512c29e670d99be93114b7d066
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2020-04-09 13:24:45 +0300

    Send emails with test results when the build_and_test_parall task finishes (refs #26792)

--- master/maxscale/services/build_and_test.py
+++ master/maxscale/services/build_and_test.py
@@ -1,31 +1,3 @@
 from . import common
 
-MAIL_TEMPLATE = common.COMPLEX_STEPS_BUILD_TEMPLATE + '''\
-
-    <h4>Test results:</h4>
-    <ol>
-    {% for step in build['steps'] %}
-        {% for test in step['triggeredBuilds'] %}
-            {% if test['testResult'] %}
-                <li>
-                <p>Result of {{ test['properties']['buildername'][0] }}</p>
-                <blockquote>
-                    <pre><p>{{ test['testResult'] }}</p></pre>
-                </blockquote>
-                {% if build['results'] == 0 %}
-                    <a href="{{
-                        'http://max-tst-01.mariadb.com/LOGS/{}-{}/LOGS/'.format(
-                            test['properties']['buildername'][0],
-                            test['properties']['buildnumber'][0]
-                        )
-                    }}">Logs(ctest logs) for each test</a>
-                {% endif %}
-                </li>
-            {% endif %}
-        {% endfor %}
-    {% endfor %}
-    </ol>
-    <p><b> -- The Buildbot</b></p>
-    '''
-
-SERVICES = [common.create_mail_notifier(MAIL_TEMPLATE, ['build_and_test'])]
+SERVICES = [common.create_mail_notifier(common.COMPLEX_TEST_RESULTS_TEMPLATE, ['build_and_test'])]
---
+++ master/maxscale/services/build_and_test_parall.py
@@ -0,0 +1,3 @@
+from . import common
+
+SERVICES = [common.create_mail_notifier(common.COMPLEX_TEST_RESULTS_TEMPLATE, ['build_and_test_parall'])]
