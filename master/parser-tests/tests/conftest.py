6bacacef1d27a7999fa351013f69b845aab6dedd
Andrey Vasilyev <andrey.vasilyev@fruct.org>
2020-06-09 12:07:03 +0300

    Remove obsolete parser tests

--- master/parser-tests/tests/conftest.py
+++
@@ -1,8 +0,0 @@
-def pytest_addoption(parser):
-    parser.addoption("--source", action="append", default=[], help="Test results location")
-
-
-def pytest_generate_tests(metafunc):
-    if 'source' in metafunc.fixturenames:
-        metafunc.parametrize("source", metafunc.config.getoption('source'),
-                             scope="session")
